1f52d683709bc6bd776666c81108654a8d012286
moad <mouadlemrani12@gmail.com>
2026-05-11 22:20:13 +0100

    commit

--- Main.java
+++ Main.java
@@ -1,1 +1,13 @@
-public class Main { public static void main(String[] args) { String q = "SELECT * FROM users WHERE id = " + args[0]; System.out.print(cc);} }
+public class Main { 
+    public static void main(String[] args) { 
+        String q = "SELECT * FROM users WHERE id = " + args[0]; 
+        System.out.print(cc);
+        public int calculate() {
+            try {
+                return 10 / 0; // Throws ArithmeticException
+            } finally {
+                return 42; // This "swallows" the exception!
+            }
+        }
+    } 
+}
